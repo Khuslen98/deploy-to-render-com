17f027e3425a069814c4f1a774fac0b66e4bb7dc
Morning <morning@Lab-02-01s-iMac.local>
2023-03-23 12:32:51 +0800

    deploy oruullaa

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cloudinary = require("./config/cloudinary");
+const uploader = require("./config/config");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +11,16 @@ const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
 
 app.use(express.json());
 app.use(cors());
+
+app.post("/upload", uploader.single("file"), async (req, res) => {
+  console.log(req);
+  const upload = await cloudinary.v2.uploader.upload(req.file.path);
+  return res.json({
+    success: true,
+    file: upload.secure_url,
+  });
+});
+
 app.get("/", (request, response) => {
   response.send(
     `<h1 style='text-align: center;color: yellowgreen; background-color: teal; font-family: fantasy;' >Hello Day-83 enviroment variable</h1>`
